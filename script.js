42eaa36a57fe86867b020f96c6b5ae94d6f494d2
ascheema4760 <38564837+ascheema4760@users.noreply.github.com>
2018-04-20 11:46:12 -0400

    added two files

---
+++ script.js
@@ -0,0 +1,21 @@
+var arrw=["cheema","bahmra"];
+
+console.log(arrw);
+console.log('dssdds');
+//var a=prompt('give the value');
+var a="helloo0000000";
+console.log(a);
+/*
+for (var i=0; i<10;i++)
+
+{
+
+    console.log(i)
+}
+*/
+var names= ["abc","def","jatt","kjnbwsd","aefe","efwef"];
+for(var i=0;i<names.length;i++)
+{
+console.log(names[i]);
+
+}
